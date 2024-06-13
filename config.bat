4f61ff3f245f9ae815183b252ddea1a668b37ea1
Nick <Xsaber64@gmail.com>
2024-06-12 23:30:46 -0500

    Removed redundancies in config.bat and added comments

--- config.bat
+++ config.bat
@@ -1,16 +1,19 @@
 echo off
-:: Set this to the directory where your game folder is located
-set "tool_install_directory=D:\Game Dev\DeploymentTools\Build Script"
+
+:: Top-level folder where your games are kept
 set "base_game_directory=D:\Game Dev\Games\Prototypes\"
+:: The subfolder which contains your .godot file
+set "game_folder=Drinks Please"
+:: Directory you installed Godot
 set "godot_directory=D:\Game Dev\Godot\godot"
+:: Location you would like to place exported builds
 set "build_exports_directory=D:\Game Dev\Games\Deployed\Drinks-Please"
 
-:: This is the 1st parameter in the script (redundant)
-set "game_folder=Drinks Please"
+:: Itch.io user where the game is published
 set "itch_user=zyjin"
+
+:: Names of the export templates in Godot for each platform
 set "windows_build_name=Windows Desktop"
-set "web_build_name=Web"
+set "web_build_name=Web - Release"
 set "mac_build_name=macOS"
 set "linux_build_name=Linux/X11"
-
-echo Setting variables for script
