f065704f1cd533aa9bdfac309723159a137974e6
Nick <Xsaber64@gmail.com>
2024-06-12 23:31:44 -0500

    Changed config defaults

--- config.bat
+++ config.bat
@@ -1,16 +1,16 @@
 echo off
 
 :: Top-level folder where your games are kept
-set "base_game_directory=D:\Game Dev\Games\Prototypes\"
+set "base_game_directory=D:\Game Dev\Games\"
 :: The subfolder which contains your .godot file
-set "game_folder=Drinks Please"
+set "game_folder=game"
 :: Directory you installed Godot
 set "godot_directory=D:\Game Dev\Godot\godot"
 :: Location you would like to place exported builds
-set "build_exports_directory=D:\Game Dev\Games\Deployed\Drinks-Please"
+set "build_exports_directory=D:\Game Dev\Games\Deployed\game"
 
 :: Itch.io user where the game is published
-set "itch_user=zyjin"
+set "itch_user=my_user"
 
 :: Names of the export templates in Godot for each platform
 set "windows_build_name=Windows Desktop"
