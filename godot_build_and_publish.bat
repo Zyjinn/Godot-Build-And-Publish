722c0fb2c69e520e6a4185533eeb9f342084d756
Nick <Xsaber64@gmail.com>
2024-06-12 23:43:21 -0500

    Added a mkdir call which creates the directories for each export template if they don't exist.

--- godot_build_and_publish.bat
+++ godot_build_and_publish.bat
@@ -11,6 +11,8 @@ cd "%base_game_directory%%game_folder%\"
 
 :: BUILD AND PUBLISH WINDOWS VERSION
 echo "Game %1 found at the directory, attempting to create windows build with template %windows_build_name%"
+echo "Creating directory \windows\ at %build_exports_directory%"
+mkdir "%build_exports_directory%\windows\"
 "%godot_directory%" --headless --export-release "%windows_build_name%"
 echo "Build complete, creating windows zip in %build_exports_directory%\windows\"
 7z a -tzip "%build_exports_directory%\windows.zip" "%build_exports_directory%\windows\*"
@@ -21,6 +23,8 @@ echo "Windows build successfully pushed to itch.io!"
 
 :: BUILD AND PUBLISH WEB VERSION
 echo "Attempting to create web build with template %web_build_name%"
+echo "Creating directory \web\ at %build_exports_directory%"
+mkdir "%build_exports_directory%\web\"
 "%godot_directory%" --headless --export-release "%web_build_name%"
 echo "Build complete, creating web zip in %build_exports_directory%\web\"
 7z a -tzip "%build_exports_directory%\web.zip" "%build_exports_directory%\web\*"
@@ -31,6 +35,8 @@ echo "Web build successfully pushed to itch.io!"
 
 :: BUILD AND PUBLISH Mac VERSION
 echo "Attempting to create Mac build with template %mac_build_name%"
+echo "Creating directory \mac\ at %build_exports_directory%"
+mkdir "%build_exports_directory%\mac\"
 "%godot_directory%" --headless --export-release "%mac_build_name%"
 echo "Build complete, creating mac zip in %build_exports_directory%\mac\"
 7z a -tzip "%build_exports_directory%\mac.zip" "%build_exports_directory%\mac\*"
@@ -41,6 +47,8 @@ echo "Mac build successfully pushed to itch.io!"
 
 :: BUILD AND PUBLISH Linux VERSION
 echo "Attempting to create linux build with template %linux_build_name%"
+echo "Creating directory \linux\ at %build_exports_directory%"
+mkdir "%build_exports_directory%\linux\"
 "%godot_directory%" --headless --export-release "%linux_build_name%"
 echo "Build complete, creating linux zip in %build_exports_directory%\linux\"
 7z a -tzip "%build_exports_directory%\linux.zip" "%build_exports_directory%\linux\*"
