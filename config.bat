054f9ccac12362f882fdf057e5729c3082795769
Nick <Xsaber64@gmail.com>
2024-09-15 12:29:17 -0500

    Updated gitignore

--- config.bat
+++ config.bat
@@ -1,21 +1,21 @@
 echo off
 
 :: Top-level folder where your games are kept
-set "base_game_directory=D:\Game Dev\Games\"
+set "base_game_directory=D:\Game Dev\Games\Prototypes\"
 :: The subfolder which contains your .godot file
-set "game_folder=game"
+set "game_folder=Birdodoro\GDProject"
 :: Location you would like to place exported builds
-set "build_exports_directory=D:\Game Dev\Games\Deployed\game"
+set "build_exports_directory=D:\Game Dev\Games\Deployed\Toridoro"
 
 :: Directory you installed Godot
-set "godot_directory=D:\Game Dev\Godot\godot"
+set "godot_directory=D:\Game Dev\Godot\Godot"
 
 
 :: Itch.io user where the game is published
-set "itch_user=my_user"
+set "itch_user=zyjin"
 
 :: Names of the export templates in Godot for each platform
 set "windows_build_name=Windows Desktop"
-set "web_build_name=Web - Release"
+set "web_build_name=Web"
 set "mac_build_name=macOS"
-set "linux_build_name=Linux/X11"
+set "linux_build_name=Linux"
